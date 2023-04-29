2a4fea345d9d30abdf4dd283edced6ecbdd7ca29
reozmen <recepozmen41@gmail.com>
2023-04-29 19:16:50 +0300

    eklemeler yapıldı

--- main.js
+++ main.js
@@ -1,36 +1,55 @@
 function krediHesapla() {
-  let cekilecekTutar, vadeSayisi, faizOrani;
+  let cekilecekTutar, faizOrani, vade, KKDF_orani, BSMV_orani;
   let aylikTaksit, odenecekToplamTutar, vergiliToplamTutar, KKDF, BSMV;
 
-  cekilecekTutar = +document.getElementById("txtKrediTutari").value;
+  cekilecekTutar = +document.getElementById("cekilecekKredi").value;
 
   faizOrani =
     +document.getElementById("faizOrani").value.replace(",", ".") / 100;
-  let vade = +document.getElementById("vade").value;
-  let KKDF_orani = parseFloat(
+
+  vade = +document.getElementById("vade").value;
+
+  KKDF_orani = parseFloat(
     document.getElementById("KKDF").value.replace(",", ".")
   );
-  let BSMV_orani = parseFloat(
+
+  BSMV_orani = parseFloat(
     document.getElementById("BSMV").value.replace(",", ".")
   );
 
-  console.log(vade);
-  console.log(faizOrani);
-  console.log(KKDF_orani);
-  console.log(BSMV_orani);
   odenecekToplamTutar =
     vade * ((cekilecekTutar * faizOrani) / (1 - 1 / (1 + faizOrani) ** vade));
 
   KKDF = (odenecekToplamTutar - cekilecekTutar) * KKDF_orani;
   BSMV = (odenecekToplamTutar - cekilecekTutar) * BSMV_orani;
+
   vergiliToplamTutar = odenecekToplamTutar + KKDF + BSMV;
 
   aylikTaksit = vergiliToplamTutar / vade;
 
-  document.querySelector("#sonuc").innerHTML =
-    "Geri ödeme toplamı: " +
-    vergiliToplamTutar.toFixed(2) +
-    "<br>" +
-    "Aylık taksit tutarınız: " +
-    aylikTaksit.toFixed(2);
+  if (!cekilecekTutar || !faizOrani || !vade) {
+    document.querySelector(
+      "#krediBilgiler"
+    ).innerHTML = `<p>Lütfen tüm alanları doldurunuz</p>`;
+  } else {
+    document.querySelector("#krediBilgiler").innerHTML =
+      `<p>Çekilecek Kredi Tutarı ➡️ <span>
+    ${cekilecekTutar.toFixed(2)}</span> </p>` +
+      "<br>" +
+      `<p>Vade Sayısı ➡️ <span>
+    ${vade}</span> </p>` +
+      "<br>" +
+      `<p>Aylık Faiz Oranı ➡️ <span>
+      ${faizOrani * 100} %</span> </p>` +
+      "<br>" +
+      `<p>Geri Ödeme Toplamı ➡️ <span>
+    ${vergiliToplamTutar.toFixed(2)}</span> </p>` +
+      "<br>" +
+      `<p>Aylık taksit tutarınız: ➡️ <span>
+    ${aylikTaksit.toFixed(2)}</span> </p>`;
+  }
+
+  document.getElementById("cekilecekKredi").value = "";
+  document.getElementById("faizOrani").value = "";
+  document.getElementById("vade").value = "";
 }
