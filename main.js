4a4e9f6f4021139cdf81442b5b3a4a859ae7af59
reozmen <recepozmen41@gmail.com>
2023-04-29 14:41:09 +0300

    main.js changed

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
-function krediHesapla()
-{
-    var cekilecekTutar,vadeSayisi;
-    var aylikTaksit,odenecekToplamTutar;
+function krediHesapla() {
+  let cekilecekTutar, vadeSayisi, faizOrani;
+  let aylikTaksit, odenecekToplamTutar, vergiliToplamTutar, KKDF, BSMV;
 
-    cekilecekTutar=document.getElementById("txtKrediTutari").value;
+  cekilecekTutar = +document.getElementById("txtKrediTutari").value;
 
-    var liste= document.getElementById("listeVade");
-    vadeSayisi=liste.options[liste.selectedIndex].value;
+  faizOrani = +document.getElementById("faizOrani").value;
+  let liste = document.getElementById("listeVade");
+  vadeSayisi = liste.options[liste.selectedIndex].value;
 
-    if(vadeSayisi==12)
-    {
-        odenecekToplamTutar=cekilecekTutar*1.1;
-    }
-    else if(vadeSayisi==24)
-    {
-        odenecekToplamTutar=cekilecekTutar*1.2;
-    }
-    else if(vadeSayisi==36)
-    {
-        odenecekToplamTutar=cekilecekTutar*1.3;
-    }
-    else if(vadeSayisi==48)
-    {
-        odenecekToplamTutar=cekilecekTutar*1.4;
-    }
-    aylikTaksit=odenecekToplamTutar/vadeSayisi;
+  console.log(faizOrani);
+  if (vadeSayisi == 12) {
+    odenecekToplamTutar =
+      12 * ((cekilecekTutar * faizOrani) / (1 - 1 / (1 + faizOrani) ** 12));
+  } else if (vadeSayisi == 24) {
+    odenecekToplamTutar =
+      24 * ((cekilecekTutar * faizOrani) / (1 - 1 / (1 + faizOrani) ** 12));
+  } else if (vadeSayisi == 36) {
+    odenecekToplamTutar =
+      36 * ((cekilecekTutar * faizOrani) / (1 - 1 / (1 + faizOrani) ** 12));
+  } else if (vadeSayisi == 48) {
+    odenecekToplamTutar =
+      48 * ((cekilecekTutar * faizOrani) / (1 - 1 / (1 + faizOrani) ** 12));
+  }
 
-    document.querySelector("#sonuc").innerHTML="Geri ödeme toplamı: "+odenecekToplamTutar+"<br>"+
-    "Aylık taksit tutarınız: "+aylikTaksit.toFixed(2);
+  KKDF = (odenecekToplamTutar - cekilecekTutar) * 0.15;
+  BSMV = (odenecekToplamTutar - cekilecekTutar) * 0.1;
+  vergiliToplamTutar = odenecekToplamTutar + KKDF + BSMV;
 
+  aylikTaksit = vergiliToplamTutar / vadeSayisi;
 
+  document.querySelector("#sonuc").innerHTML =
+    "Geri ödeme toplamı: " +
+    vergiliToplamTutar.toFixed(2) +
+    "<br>" +
+    "Aylık taksit tutarınız: " +
+    aylikTaksit.toFixed(2);
 }
